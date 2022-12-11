ad52def75f974d05067b4f7d8c7d033d9169ce35
Wanda <wandaplaza1031@gmail.com>
2022-12-11 14:47:37 -0500

    Issue

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function firstNameLetter(name) {
-    alert("The name " + name + "starts with the letter " + firstLetterOfName);
+    let firstLetterOfName = name[0]
+    alert("The name " + name + " starts with the letter " + firstLetterOfName);
 }
 
 function divisibleByTwo (x) {
@@ -11,15 +12,17 @@ function divisibleByTwo (x) {
     console.log(x)
 }
 
-const number = [8, 15, 4, 21, 33]
-function largestNum(arr) {
-    for(let i = 0; i < number.length; i++) {
-        if(arr[0] < arr[i]) {
-            arr[0] = arr[i]
-        }
+function largestNum(number) {
+    const largest = [0];
+for (let i = 0; i < number.length; i++) {
+    if (largest < number[i]) {
+        largest = number[i];
     }
+return number
+}
 }
 
 console.log("The number 5 is divisible by 2: " + divisibleByTwo(5));
 console.log("The number 8 is divisible by 2: " + divisibleByTwo(8));
-console.log(largestNum(23, 5, 11, 6, 7))
+console.log(largestNum([15, 5, 8, 3, 7]))
+firstNameLetter('Wanda')
